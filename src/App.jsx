0b91237a842a3b389a3f491b2add35bb1146db49
Acer-Aspire-3 <1zeydinik@gmail.com>
2025-05-28 02:59:19 +0200

    Add HTML & CSS  to main element

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
 import './App.scss';
+/* icons */
+import { GoGoal } from 'react-icons/go';
+import { LuStar } from 'react-icons/lu';
+import { TiTickOutline } from 'react-icons/ti';
+import { TbTrashX } from 'react-icons/tb';
 
 function App() {
   return (
@@ -55,8 +60,67 @@ function App() {
             </button>
           </div>
         </form>
-        <hr />
+        <hr className="hr" />
       </header>
+      <main className="task_columns_container">
+        <section className="task_column">
+          <h2>
+            <GoGoal className="goal_icon" />{' '}
+            <span className="status_text">To do</span>
+          </h2>
+          <div className="card">
+            <div className="title">sjdhdds</div>
+            <div className="bottom_container">
+              <div className="buttons_container">
+                <button className="tag_btn">HTML</button>
+                <button className="tag_btn">CSS</button>
+                <button className="tag_btn">JavaScript</button>
+              </div>
+              <span className="delete_icon">
+                <TbTrashX />
+              </span>
+            </div>
+          </div>
+        </section>
+        <section className="task_column">
+          <h2>
+            <GoGoal className="goal_icon" />{' '}
+            <span className="status_text">To do</span>
+          </h2>
+          <div className="card">
+            <div className="title">ddddsssssssssssssssssssssssssssssssssdd</div>
+            <div className="bottom_container">
+              <div className="buttons_container">
+                <button className="tag_btn">HTML</button>
+                <button className="tag_btn">CSS</button>
+                <button className="tag_btn">JavaScript</button>
+              </div>
+              <span className="delete_icon">
+                <TbTrashX />
+              </span>
+            </div>
+          </div>
+        </section>
+        <section className="task_column">
+          <h2>
+            <GoGoal className="goal_icon" />{' '}
+            <span className="status_text">To do</span>
+          </h2>
+          <div className="card">
+            <div className="title">sjdhdds</div>
+            <div className="bottom_container">
+              <div className="buttons_container">
+                <button className="tag_btn">HTML</button>
+                <button className="tag_btn">CSS</button>
+                <button className="tag_btn">JavaScript</button>
+              </div>
+              <span className="delete_icon">
+                <TbTrashX />
+              </span>
+            </div>
+          </div>
+        </section>
+      </main>
     </div>
   );
 }
